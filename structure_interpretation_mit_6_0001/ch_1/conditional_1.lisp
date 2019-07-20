e85ec1a7bbf438a08c49af1a66850aad05783b07
Damon Jansen <djansen271@gmail.com>
2019-07-20 12:01:07 -0700

    Add sqrt ch 1

--- structure_interpretation_mit_6_0001/ch_1/conditional_1.lisp
+++ structure_interpretation_mit_6_0001/ch_1/conditional_1.lisp
@@ -9,3 +9,40 @@
 (abs -2)
 (abs 0)
 (abs 12)
+
+( define(abs_a_plus_2b a b )
+  ((if (< a 0) - +) )
+)
+
+; Exercise 1.5
+
+;(define (p) (p))
+;(define (test x y ))
+;  (if (= x 0) 0 y)
+
+
+(define (improve guess x)
+  (avg guess (/ x guess))
+)
+
+
+(define (avg a b)
+  (/ (+ a b ) 2)
+ )
+
+(avg 12 0)
+;6
+(avg 12 2)
+;7
+(avg 12 4)
+;8
+(avg (- 8) 8)
+;0
+
+(improve 1 2)
+;1.5
+(improve 1.5 2)
+
+(define (sqrt guess x)
+  ()
+)
